f803e4b83feaa78d963f3dafaf103be89adcaa7b
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-02-25 01:36:41 +0800

    re-add cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('sohafidz'),
+      api_key: env('219784346579941'),
+      api_secret: env('yt2Pz_ZZlcu1GZftCo3M7dkWCyo'),
+    },
+  },
+  // ...
+});
