0ef8fd47896321573e33d8edd29dc3bbb255105f
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-02-25 01:55:04 +0800

    use env var for secrets

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-      cloud_name: env('sohafidz'),
-      api_key: env('219784346579941'),
-      api_secret: env('yt2Pz_ZZlcu1GZftCo3M7dkWCyo'),
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
     },
   },
   // ...
